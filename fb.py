7cb6c14c3c44228d5de2a15a25994ff63572d68f
shashank0610 <44138672+shashank0610@users.noreply.github.com>
2018-10-14 19:39:43 +0530

    Create fb.py

---
+++ fb.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+from selenium.webdriver.common.action_chains import ActionChains
+
+browser = webdriver.Firefox()
+browser.get('https://www.facebook.com/')
+
+user = input("Enter FB user mail")
+password = input("Enter password")
+
+elem = browser.find_element_by_id("email")
+elem.send_keys(user)
+
+elem = browser.find_element_by_id("pass")
+elem.send_keys(password + Keys.ENTER)
+
+print("Logged into FB")
